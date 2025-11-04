f16b253345b9972fc023b8e8c2d73c860748baef
KietVu <tuankiet.vtnktv@gmail.com>
2025-11-04 17:59:31 +0700

    Add project files.

---
+++ APP_QLHocPhi/NVVM/ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,81 @@
+﻿using APP_QLHocPhi.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace APP_QLHocPhi.NVVM.ViewModel
+{
+    public class ControlBarViewModel : BaseViewModel
+    {
+        #region commands
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MaximizeWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommand { get; set; }
+
+        public ICommand MouseMoveWindowCommand { get; set; }
+
+        #endregion
+
+        public ControlBarViewModel()// navigate đến windows cuối cùng để xoá 
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    w.Close();
+                }
+            });
+            MaximizeWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    if(w.WindowState != WindowState.Maximized)
+                        w.WindowState = WindowState.Maximized;
+                    else
+                        w.WindowState = WindowState.Normal;
+                }
+            });
+            MinimizeWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                   if(w.WindowState != WindowState.Minimized)
+                        w.WindowState = WindowState.Minimized;
+                   else
+                        w.WindowState = WindowState.Maximized;
+                }
+            });
+            MouseMoveWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                FrameworkElement window = GetWindowParent(p);
+                var w = window as Window;
+                if (w != null)
+                {
+                    w.DragMove();
+                }
+            });
+        }
+        public FrameworkElement GetWindowParent(UserControl p) // hàm lấy parent của user control
+        {
+            FrameworkElement parent = p; //cho parent bằng chính user controll
+
+            while (parent != null && parent.Parent is FrameworkElement fe) // lặp đến khi parent ko còn parent nữa
+            {
+                parent = fe; // gán parent mới
+            }
+
+            return parent;
+        }
+    }
+}
---
+++ APP_QLHocPhi/NVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using APP_QLHocPhi.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace APP_QLHocPhi.NVVM.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool Isloaded = false;
+
+        public ICommand LoadedWindowCommand { get; set; }
+
+        //tất cả sẽ được sử lí trong đây    
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                //Isloaded = true;
+                //LoginWindow loginWindow = new LoginWindow();
+                //loginWindow.ShowDialog();
+
+                var main = Application.Current.MainWindow as Window;
+                main?.Hide();
+
+                var login = new LoginWindow
+                {
+                    Owner = main,
+                    WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                    Topmost = true
+                };
+
+                // Khi login đóng (dù là bấm X hay login thành công)
+                login.Closed += (s, e) =>
+                {
+                    main?.Show();
+                    main?.Activate();
+                };
+
+                login.Show();
+            });           
+        }
+    }
+}
