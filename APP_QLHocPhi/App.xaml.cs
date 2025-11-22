2f3a70a265059099deab4eb17501fc5fee821e24
KietVu <tuankiet.vtnktv@gmail.com>
2025-11-22 09:21:22 +0700

    TestSuject

--- APP_QLHocPhi/App.xaml.cs
+++ APP_QLHocPhi/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
+using System.IO;
+using System.Reflection;
 using System.Windows;
 
 namespace APP_QLHocPhi
@@ -9,6 +11,15 @@ namespace APP_QLHocPhi
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Thiết lập đường dẫn DataDirectory về nơi file .exe đang chạy
+            string executable = Assembly.GetExecutingAssembly().Location;
+            string path = Path.GetDirectoryName(executable);
+            AppDomain.CurrentDomain.SetData("DataDirectory", path);
+        }
     }
 
 }
