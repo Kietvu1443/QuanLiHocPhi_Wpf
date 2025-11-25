ff975b715dc90b0f475ad3b747d2f3a2c8db530d
KietVu <tuankiet.vtnktv@gmail.com>
2025-11-25 14:48:38 +0700

    Add schoolfee

---
+++ APP_QLHocPhi/QRWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace APP_QLHocPhi
+{
+    public partial class QRWindow : Window
+    {
+        public QRWindow(string qrUrl)
+        {
+            InitializeComponent();
+            LoadQrImage(qrUrl);
+        }
+
+        void LoadQrImage(string url)
+        {
+            try
+            {
+                // Tạo ảnh từ đường dẫn URL
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(url, UriKind.Absolute);
+                bitmap.EndInit();
+                QrImage.Source = bitmap;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Không tải được mã QR. Vui lòng kiểm tra kết nối mạng!");
+            }
+        }
+    }
+}
