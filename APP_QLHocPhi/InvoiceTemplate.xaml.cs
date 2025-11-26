8258ef269f4b469740d971b82edaf4a0a090ca26
KietVu <tuankiet.vtnktv@gmail.com>
2025-11-26 19:46:16 +0700

    Added Invoiced

---
+++ APP_QLHocPhi/InvoiceTemplate.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace APP_QLHocPhi
+{
+    /// <summary>
+    /// Interaction logic for InvoiceTemplate.xaml
+    /// </summary>
+    public partial class InvoiceTemplate : UserControl
+    {
+        public InvoiceTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
